a8ad8a487a4a50e6c30b872a2561a72fb5cf4c41
Jeff Auriemma <jdauriemma@gmail.com>
2019-03-26 16:57:39 -0400

    Commit 3: layout tags

--- src/App.js
+++ src/App.js
@@ -5,24 +5,50 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <img
-        src={pepper}
-        alt='Pepper'
-      />
-      <h2>
-        Pepper
-      </h2>
-      <p>
-        Pepper is a domestic shorthair.  This is funny because she has
-        long hair in real life.  She enjoys being pet until she's done with
-        that and bites me.  She also seems to like meetings, because she
-        joins almost all of them.
-      </p>
-      <button
-        type='button'
-      >
-        Pet Pepper
-      </button>
+      <div>
+        <section>
+          <div>
+            <img
+              src={pepper}
+              alt='Pepper'
+            />
+          </div>
+        </section>
+        <section>
+          <header>
+            <h2>
+              <span>
+                Pepper
+              </span>
+            </h2>
+          </header>
+          <section>
+            <p>
+              <span>
+                Pepper is a domestic shorthair.  This is funny because she has
+                long hair in real life.  She enjoys being pet until she's done with
+                that and bites me.  She also seems to like meetings, because she
+                joins almost all of them.
+              </span>
+            </p>
+          </section>
+          <footer>
+            <div>
+              <div>
+                <button
+                  type='button'
+                >
+                  <div>
+                    <span>
+                      Pet Pepper
+                    </span>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </footer>
+        </section>
+      </div>
     </div>
   );
 };
